6b6a2783a6042670c0c30846c6d13d1a1311d73a
minercool <zghidig@gmail.com>
2021-08-28 01:21:31 +0100

    udpate

--- api/controllers/contratController.js
+++ api/controllers/contratController.js
@@ -93,3 +93,43 @@ router.post("/post", async (req, res) => {
     res.status(500).send("server error");
   }
 });
+
+router.patch("/update/:id", async (req, res) => {
+  try {
+    connection.query(
+      "UPDATE contrat SET numero = ?, client = ?, article = ?,qte = ?, montant = ?,avance = ?, datecontrat = ?,premiereecheance = ?, nbrecheance = ? ,mensualite = ?, 2annuite = ?, 2mensualite = ?, commission_vendeur = ?, recouvreur = ?,remarque = ?, chef = ?, comm = ?, statucontrat = ?, active = ? WHERE id = ?",
+      [
+        req.body.numero,
+        req.body.client,
+        req.body.article,
+        req.body.qte,
+        req.body.montant,
+        req.body.avance,
+        req.body.datecontrat,
+        req.body.premiereecheance,
+        req.body.nbrecheance,
+        req.body.mensualite,
+        req.body.annuite2,
+        req.body.mensualite2,
+        req.body.commission_vendeur,
+        req.body.recouvreur,
+        req.body.remarque,
+        req.body.chef,
+        req.body.comm,
+        req.body.statucontrat,
+        req.body.active,
+        req.params.id
+      ],
+      (error, rows, fields) => {
+        if (rows.affectedRows != 0) {
+          res.status(200).send(rows);
+        } else {
+          res.status(500).send(error);
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server error");
+  }
+});
